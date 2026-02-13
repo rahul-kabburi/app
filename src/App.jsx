56c9bf296571125b7f91e6046eeb237d4f08bddf
Rahul Kabburi <rahul.rkabburi@gmail.com>
2026-02-13 22:39:53 +0530

    Move haa to Maybe category

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,12 @@ export default function App() {
     'Yes': [
       { file: 'ofcourse.jpg' },
       { file: 'definately.jpg' },
-      { file: 'haa.jpg' }
+      
     ],
     'Maybe': [
       { file: 'formality.jpg' },
       { file: 'yawn.jpg' }
+      { file: 'haa.jpg' }
     ],
     'No': [
       { file: 'no.jpg' },
